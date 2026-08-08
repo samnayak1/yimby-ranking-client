d5708aedf4666af43e84a5ff0f85ebd73f59bb8c
Samasth Nayak <samasthnayak1@gmail.com>
2026-08-08 16:14:49 +0000

    changed vite route

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
- 
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:3000',
+      "/api": {
+        target: "http://[::1]:80",
+        changeOrigin: true,
+      },
     },
   },
 });
